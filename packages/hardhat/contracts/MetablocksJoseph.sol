62b3059c3d0b71f44af3a11b8b61b9849a86c9c4
Danilo Ramirez <daramir9999@gmail.com>
2021-04-11 22:34:33 +1000

    update to joseph game and dice roller contracts

--- packages/hardhat/contracts/MetablocksJoseph.sol
+++ packages/hardhat/contracts/MetablocksJoseph.sol
@@ -3,17 +3,32 @@ pragma solidity >=0.7.6;
 
 import {DiceRoll} from "./DiceRoll.sol";
 
-contract MetablocksJoseph  {
-    
+contract MetablocksJoseph {
     address private owner;
-
     DiceRoll private diceRollPlugin;
-    
+    string[] private avatars;
+
+    // Event Definitions
+    event PlayerJoined(
+        string indexed gameName,
+        address indexed playerAddress,
+        string indexed avatar,
+        string message
+    );
+
+    // Event Definitions
+    event PlayerStartedTurn(
+        string indexed gameName,
+        address indexed playerAddress,
+        uint256 newPosition,
+        string message
+    );
+
     constructor(address diceRollContract) {
         owner = msg.sender;
 
         diceRollPlugin = DiceRoll(diceRollContract);
-        
+
         // Create land
         createLand(1, "Mayfair", 80);
         createLand(3, "Adelaide", 80);
@@ -21,57 +36,66 @@ contract MetablocksJoseph  {
         createLand(5, "VLINE", 98);
         createLand(6, "Melbourne", 200);
         createLand(8, "Mildura", 200);
-        
+
+        // Create available avatars
+        avatars.push("LedgerNano");
+        avatars.push("BowTie");
+        avatars.push("Ghost");
+
         // Step 1: Create player who wants to host game
         createPlayer("jrocco2");
         // Step 2: Create game
-        createGame("JoesGame", 2);        
+        createGame("JoesGame", 2);
     }
-    
+
     // ----- PLAYERS ENTITY -----
     struct Player {
         string username;
         address playerAddress;
         uint256 balance;
         uint256 positionOnBoard;
-        address gameAddress;
+        address gameHostAddress;
+        string avatar;
     }
-    
+
     function createPlayer(string memory username) public {
         Player memory myPlayer;
         myPlayer.username = username;
         myPlayer.playerAddress = msg.sender;
         playerMapping[msg.sender] = myPlayer;
     }
-    
+
     function setupPlayer(string memory gameName) private {
         // Setup player
         //playerMapping[msg.sender] is a Player
-        playerMapping[msg.sender].gameAddress = hostMapping[gameName];
+        playerMapping[msg.sender].gameHostAddress = hostMapping[gameName];
         playerMapping[msg.sender].positionOnBoard = 0;
         // Monoply Rule: Each player is given $1500
-         playerMapping[msg.sender].balance = 1500;
+        playerMapping[msg.sender].balance = 1500;
     }
-    
+
     mapping(address => Player) public playerMapping;
-    
+
     // ----- GAME ENTITY -----
     struct Game {
-        string name; 
+        string name;
         int256 timePerMove;
         bool hasStarted;
         address creatorAddress;
         address[] players;
         Player currentPlayer;
     }
-    
+
     // mapping this way means one player can only host one game
     // and therefore must remove their game in order to create another
     mapping(address => Game) private gameMapping;
     mapping(string => address) private hostMapping;
-    
+
     function createGame(string memory name, int256 timePerMove) public {
-        require(playerMapping[msg.sender].playerAddress == msg.sender, "You have to create your player before you can create a game");
+        require(
+            playerMapping[msg.sender].playerAddress == msg.sender,
+            "You have to create your player before you can create a game"
+        );
         Game memory myGame;
         myGame.name = name;
         myGame.timePerMove = timePerMove;
@@ -79,80 +103,195 @@ contract MetablocksJoseph  {
         myGame.creatorAddress = msg.sender;
         gameMapping[msg.sender] = myGame;
         gameMapping[msg.sender].players.push(msg.sender);
-        
+        playerMapping[msg.sender].avatar = avatars[
+            gameMapping[msg.sender].players.length
+        ];
         hostMapping[name] = msg.sender;
-        
+
         setupPlayer(name);
-        
     }
-    
+
     function joinGame(string memory name) public {
         require(hostMapping[name] != address(0), "Invalid name");
-        require(hostMapping[name] != playerMapping[msg.sender].gameAddress, "You have already joined this game");
+        require(
+            hostMapping[name] != playerMapping[msg.sender].gameHostAddress,
+            "You have already joined this game"
+        );
         // Monoply Rule: There is usually 8 players in a game (but works with more)
-        require(gameMapping[hostMapping[name]].players.length < 8, "This game has reached the maximum number of players");
-        require(gameMapping[hostMapping[name]].hasStarted == false, "The game has already started");
-        require(playerMapping[msg.sender].playerAddress == msg.sender, "You have to create your player before you can join a game");
-        gameMapping[hostMapping[name]].players.push(msg.sender); 
-        
+        require(
+            gameMapping[hostMapping[name]].players.length < 8,
+            "This game has reached the maximum number of players"
+        );
+        require(
+            gameMapping[hostMapping[name]].hasStarted == false,
+            "The game has already started"
+        );
+        require(
+            playerMapping[msg.sender].playerAddress == msg.sender,
+            "You have to create your player before you can join a game"
+        );
+        gameMapping[hostMapping[name]].players.push(msg.sender);
+
         setupPlayer(name);
     }
-    
-    
-    
+
     function startGame() public {
         // require(gameMapping[hostMapping[name]].creatorAddress == msg.sender,"Only the host can start the game they have created");
-        require(gameMapping[msg.sender].creatorAddress != address(0), "You are not hosting any games");
-        require(gameMapping[msg.sender].players.length >= 2, "Not enough players");
+        require(
+            gameMapping[msg.sender].creatorAddress != address(0),
+            "You are not hosting any games"
+        );
+        require(
+            gameMapping[msg.sender].players.length >= 2,
+            "Not enough players"
+        );
         gameMapping[msg.sender].hasStarted = true;
         //TODO: DO VRF HERE TO CHOOSE WHO PLAYS FIRST, atm it's fifo
-        gameMapping[msg.sender].currentPlayer = playerMapping[gameMapping[msg.sender].players[0]];
+        gameMapping[msg.sender].currentPlayer = playerMapping[
+            gameMapping[msg.sender].players[0]
+        ];
         // AND ROLL DICE FOR PLAYER
         diceRollPlugin.rollDice();
-
     }
-    
+
     function endGame() public {
-        require(gameMapping[msg.sender].creatorAddress != address(0), "You are not hosting any games");
+        require(
+            gameMapping[msg.sender].creatorAddress != address(0),
+            "You are not hosting any games"
+        );
         gameMapping[msg.sender].hasStarted = false;
     }
-    
+
     function removeGame(string memory name) private {
-        require(gameMapping[hostMapping[name]].creatorAddress == msg.sender || owner == msg.sender,"Only the host can remove the game they have created");
+        require(
+            gameMapping[hostMapping[name]].creatorAddress == msg.sender ||
+                owner == msg.sender,
+            "Only the host can remove the game they have created"
+        );
         delete gameMapping[hostMapping[name]];
         delete hostMapping[name];
     }
 
-    function showMeMyGame() public view returns (string memory, int256, address, address[] memory) {
+    function showMeMyGame()
+        public
+        view
+        returns (
+            string memory,
+            int256,
+            address,
+            address[] memory
+        )
+    {
         Game memory myGame = gameMapping[msg.sender];
-        return (myGame.name, myGame.timePerMove, myGame.creatorAddress, myGame.players);
+        return (
+            myGame.name,
+            myGame.timePerMove,
+            myGame.creatorAddress,
+            myGame.players
+        );
     }
-    
+
     // ----- GAME FUNCTIONALITY -----
-    
+
     function rollDice() public {
-        require(playerMapping[msg.sender].gameAddress != address(0), "Player has not joined a game");
+        require(
+            playerMapping[msg.sender].gameHostAddress != address(0),
+            "Player has not joined a game"
+        );
         // VRF rollDice then......
         // playerMapping[msg.sender].positionOnBoard += diceRoll;
         // NEXT PLAYERS TURN
     }
-    
+
     function pickUpCard() public {
-        require(playerMapping[msg.sender].gameAddress != address(0), "Player has not joined a game");
+        require(
+            playerMapping[msg.sender].gameHostAddress != address(0),
+            "Player has not joined a game"
+        );
         // Pick up card and do some function based on card
     }
-    
-    function buyLand() public {
-        
+
+    function getRollStartTurn() public {
+        require(
+            playerMapping[msg.sender].gameHostAddress != address(0),
+            "Player has not joined a game"
+        );
+
+        require(
+            gameMapping[playerMapping[msg.sender].gameHostAddress]
+                .currentPlayer
+                .playerAddress == msg.sender,
+            "It's not your turn"
+        );
+        playerMapping[msg.sender].positionOnBoard = calculateGameTile(
+            playerMapping[msg.sender].positionOnBoard,
+            diceRollPlugin.getMostRecentRoll()
+        );
+
+        emit PlayerStartedTurn(
+            gameMapping[playerMapping[msg.sender].gameHostAddress].name,
+            msg.sender,
+            playerMapping[msg.sender].positionOnBoard,
+            playerMapping[msg.sender].username
+        );
     }
-    
-    function swapLand() public {
-        
+
+    function endTurn() public {
+        require(
+            playerMapping[msg.sender].gameHostAddress != address(0),
+            "Player has not joined a game"
+        );
+
+        require(
+            gameMapping[playerMapping[msg.sender].gameHostAddress]
+                .currentPlayer
+                .playerAddress == msg.sender,
+            "It's not your turn"
+        );
+
+        Player memory next =
+            playerMapping[
+                calculateNextPlayer(
+                    gameMapping[playerMapping[msg.sender].gameHostAddress],
+                    gameMapping[playerMapping[msg.sender].gameHostAddress]
+                        .currentPlayer
+                        .playerAddress
+                )
+            ];
+        gameMapping[playerMapping[msg.sender].gameHostAddress]
+            .currentPlayer = next;
+
+        diceRollPlugin.rollDice();
     }
-    
-    function payDebt() public {
-        
+
+    function calculateGameTile(uint256 currentPosition, uint8 diceRoll)
+        private
+        returns (uint256)
+    {
+        return (currentPosition + uint256(diceRoll)) % 40;
     }
+
+    function calculateNextPlayer(Game storage _game, address currentPlayerAddr)
+        private
+        returns (address)
+    {
+        for (uint8 index = 0; index < _game.players.length; index++) {
+            if (_game.players[index] == currentPlayerAddr) {
+                if (index == _game.players.length - 1) {
+                    return _game.players[0];
+                } else {
+                    return _game.players[index + 1];
+                }
+            }
+        }
+        return _game.players[0];
+    }
+
+    function buyLand() public {}
+
+    function swapLand() public {}
+
+    function payDebt() public {}
 
     // ----- LAND ENTITY -----
     struct Land {
@@ -164,15 +303,36 @@ contract MetablocksJoseph  {
 
     mapping(int32 => Land) public landMapping;
     int32[] public landList; // The order of the list is the order of Land on the Monopoly board
-    
-    function createLand(int32 _landTileId, string memory landName, int256 landValue) private {
-        landMapping[_landTileId] = Land(_landTileId, landName, landValue, owner);
+
+    function createLand(
+        int32 _landTileId,
+        string memory landName,
+        int256 landValue
+    ) private {
+        landMapping[_landTileId] = Land(
+            _landTileId,
+            landName,
+            landValue,
+            owner
+        );
         landList.push(_landTileId);
     }
-    
-    function getLand(int32 _landTileId) public view returns (int32, string memory, int256) {
-        return (landMapping[_landTileId].landTileId, landMapping[_landTileId].name, landMapping[_landTileId].value);
+
+    function getLand(int32 _landTileId)
+        public
+        view
+        returns (
+            int32,
+            string memory,
+            int256
+        )
+    {
+        return (
+            landMapping[_landTileId].landTileId,
+            landMapping[_landTileId].name,
+            landMapping[_landTileId].value
+        );
     }
-    
+
     //  -------------------------------
 }
